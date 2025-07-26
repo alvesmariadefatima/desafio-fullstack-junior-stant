e655b2c47a1eeb4be437ffaf16e52d82710ccc6d
Maria de Fátima <mnunesalves334@gmail.com>
2025-07-26 11:43:35 -0300

    feat: Criação do componente Banner

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,19 @@
+const Banner = () => {
+    return (
+        <>
+            <img 
+                src="src/assets/BANNER DESKTOP.png" 
+                alt="Banner da Pizzaria (Versão Desktop)"
+                className="hidden md:block w-full"
+            />
+
+            <img 
+                src="src/assets/BANNER MOBILE.png" 
+                alt="Banner da Pizzaria SVG (Versão Mobile)" 
+                className="block md:hidden w-full"
+            />
+        </>
+    )
+}
+
+export default Banner
